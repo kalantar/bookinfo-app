e275038f9602effe8f7ba23f15c908eea39267a9
Michael Kalantar <kalantar@us.ibm.com>
2019-05-31 20:30:20 -0400

    try scripts repo

---
+++ iter8/bin/generate_load.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+FREQUENCY=2
+OPTIONS="-Is"
+DURATION="10s"
+URL=
+
+# process options
+while [ $# -gt 0 ]; do
+  case "${1}" in
+    --url)
+      URL="${2}"
+      shift; shift
+      ;;
+    -f|--frequency)
+      FREQUENCY="${2}"
+      shift; shift
+      ;;
+    -d|--duration)
+      DURATION="${2}"
+      shift; shift
+      ;;
+    -o|--options)
+      OPTIONS="${2}"
+      shift; shift
+      ;;
+    *) echo "WARNING: Ignoring unknown option: ${1}"
+      ;;
+  esac
+done
+
+echo "URL       = $URL"
+echo "OPTIONS   = $OPTIONS"
+echo "DURATION  = $DURATION"
+echo "FREQUENCY = $FREQUENCY"
+
+generate {
+  while sleep $FREQUENCY ; do
+    curl $OPTIONS $URL
+  done
+}
+
+#timeout $DURATION generate
+#timeout $DURATION while sleep $FREQUENCY ; do curl $OPTIONS $URL ; done
+while sleep $FREQUENCY ; do curl $OPTIONS $URL ; done
